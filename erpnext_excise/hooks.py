998ff4b0b7262a2916b4ca8342364b301b29fd00
Federico Dolce <psykopear@gmail.com>
2017-07-18 13:43:24 +0200

    Add purchase and sales invoice"

--- erpnext_excise/hooks.py
+++ erpnext_excise/hooks.py
@@ -89,10 +89,10 @@ app_license = "MIT"
 
 fixtures = ["Custom Field"]
 doc_events = {
-	"Sales Order": {
+	("Sales Order", "Sales Invoice"): {
 		"before_save": "erpnext_excise.erpnext_excise.add_excise.sales_order_excise"
 	},
-	"Purchase Order": {
+	("Purchase Order", "Purchase Invoice"): {
 		"before_save": "erpnext_excise.erpnext_excise.add_excise.purchase_order_excise"
 	}
 }
